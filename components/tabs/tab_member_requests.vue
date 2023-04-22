4d4f364c7e867d1f4e041ae651f415949b0d85d7
Elirehema Paul <rocksandystone@gmail.com>
2023-04-22 22:30:34 +0300

    Implement approval and rejects

--- components/tabs/tab_member_requests.vue
+++ components/tabs/tab_member_requests.vue
@@ -10,9 +10,11 @@
         :server-items-length="pages"
         @pagination="paginate"
       >
-      <template #item.status="{ item }">
-      <v-chip v-if="item.status != '-1'" small>{{ item.status }}</v-chip>
-      </template>
+        <template #item.status="{ item }">
+          <v-chip v-if="item.status != '-1'" small>
+            {{ item.status }}
+          </v-chip>
+        </template>
         <template #top>
           <v-toolbar color="" flat>
             <v-toolbar-title class="text-uppercase">
@@ -31,9 +33,30 @@
             <span>{{ item.description }}</span>
           </v-tooltip>
         </template>
+        <template #item.actions="{ item }">
+          <v-btn-toggle
+            v-if="item.status != '-1' && item.status == 'PENDING'"
+            v-model="toggle_exclusive"
+            dense
+            rounded
+          >
+            <v-btn small style="background-color: blue !important; text-transform: none !important;" @click="approveRequest(item, 'approved')">
+              <v-icon color="white" left>
+                mdi-check
+              </v-icon>
+              Approve
+            </v-btn>
+            <v-btn small v-if="item.status != '-1' && item.status == 'PENDING'" style="background-color: red !important; text-transform: none !important;" @click="approveRequest(item, 'rejected')">
+              <v-icon color="white" left>
+                mdi-close
+              </v-icon>
+              <span>Reject</span>
+            </v-btn>
+          </v-btn-toggle>
+        </template>
         <template #item.type="{ item }">
-        <span v-if="item.transactionType != null"> {{  item.transactionType.type }}</span>
-        <span v-else class="grey--text"> Not Provided </span>
+          <span v-if="item.transactionType != null"> {{ item.transactionType.type }}</span>
+          <span v-else class="grey--text"> Not Provided </span>
         </template>
         <template #no-data>
           <span>No transaction found ...</span>
@@ -56,7 +79,8 @@ export default {
         { text: 'RequestType', value: 'type' },
         { text: 'Description', value: 'desc' },
         { text: 'Confirm Date ', value: 'confirmDate' },
-        { text: 'Request Date', value: 'logDate' }
+        { text: 'Request Date', value: 'logDate' },
+        { text: 'Actions', value: 'actions' }
       ],
       show: false,
       editedItem: {}
@@ -92,6 +116,14 @@ export default {
           this.pages = response.totalRows
           this.page = response.currentPage
           this.approvals = response.results == null ? [] : response.results
+        })
+        .catch((_err) => {})
+    },
+    async approveRequest (val, act) {
+      await this.$api
+        .$put(`/members/approvals/${val.id}`, null, { params: { action: act } })
+        .then(() => {
+          this.paginate({ page: 0, itemsPerPage: 15 })
         })
         .catch((_err) => {})
     }
